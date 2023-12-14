964cc2b217cf8a8887f2b0d35a0bde38dfebda01
aamir <aamirrazdan786@gmail.com>
2023-12-14 16:58:58 +0530

    message

--- 04basic/src/App.jsx
+++ 04basic/src/App.jsx
@@ -1,10 +1,20 @@
 
-
+import './index.css'
+// import Price from './components/Price'
+import Red from './components/Red';
 
 function App() {
  
   return (
-   <h1>joker</h1>
+   <>
+{/*   
+   <Price price="$400"/>
+   <Price /> */}
+   <div className="buttons">
+   <Red />
+
+   </div>
+   </>
   )
 }
 
---
+++ 04basic/src/components/Red.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+export default function Red() {
+  return (
+    <div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-5 rounded">click me</button>
+      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mr-5 rounded">click me</button>
+      <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 mr-5 rounded">click me</button>
+      <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 mr-5 rounded">click me</button>
+      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mr-5 rounded">click me</button>
+    </div>
+  )
+}
+
